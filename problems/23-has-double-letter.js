f438f14502ed04d5417992c6067424918493d110
robweekes <weekes.robert@gmail.com>
2024-05-14 20:00:19 -0700

    23 and 24

--- problems/23-has-double-letter.js
+++ problems/23-has-double-letter.js
@@ -5,15 +5,31 @@ character consecutively. If the value passed into the function is not a string,
 return null.
 */
 
-// Your code here 
+const hasDoubleLetter = str => {
+    if(typeof str !== 'string') {
+        return null;
+    }
+    for(let i = 0; i < str.length; i++) {
+        let char1 = str[i];
+        console.log(char1);
+        let char2 = str[i + 1];
+        console.log(char2);
 
-// console.log(hasDoubleLetter('deer')); // true
-// console.log(hasDoubleLetter('boot camp')); // true
-// console.log(hasDoubleLetter('toggle')); // true
-// console.log(hasDoubleLetter('taco')); // false
-// console.log(hasDoubleLetter('jumper')); // false
-// console.log(hasDoubleLetter(18)); // null
-// console.log(hasDoubleLetter(['array'])); // null
+        if(char2 === char1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(hasDoubleLetter('deer')); // true
+console.log(hasDoubleLetter('boot camp')); // true
+console.log(hasDoubleLetter('toggle')); // true
+console.log(hasDoubleLetter('taco')); // false
+console.log(hasDoubleLetter('jumper')); // false
+console.log(hasDoubleLetter(18)); // null
+console.log(hasDoubleLetter(['array'])); // null
+
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = hasDoubleLetter;
--- problems/24-double-letter-count.js
+++ problems/24-double-letter-count.js
@@ -3,10 +3,28 @@ Write a function doubleLetterCount that takes in a string and returns the number
 of times that the same letter repeats twice in a row.
 */
 
-// Your code here 
+const doubleLetterCount = str => {
+    let count = 0;
 
-// console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
-// console.log(doubleLetterCount("bootcamp")); // 1
+    for(let i = 0; i < str.length; i++) {
+        let char1 = str[i];
+        console.log(char1);
+        let char2 = str[i + 1];
+        console.log(char2);
+
+        if(char2 === char1) {
+            count++;
+            console.log(count);
+        }
+    }
+    return count;
+}
+
+
+
+console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+console.log(doubleLetterCount("bootcamp")); // 1
+
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = doubleLetterCount;
